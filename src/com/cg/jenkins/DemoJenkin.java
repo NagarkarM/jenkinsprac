567d94d41d1ee55e07adbdf7c10a6858c0fffc99
github_mn <manjusha.nagarkar@gmail.com>
2022-03-01 16:59:38 +0530

    Second Commit

--- src/com/cg/jenkins/DemoJenkin.java
+++ src/com/cg/jenkins/DemoJenkin.java
@@ -3,9 +3,11 @@ package com.cg.jenkins;
 public class DemoJenkin {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello Jenkin!!");
 		System.out.println("Hi");
 
+		
 	}
 
 }
